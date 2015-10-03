902fa01c811dcc15bfb02eece20628b99d706c15
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-10-03 15:36:02 +0300

    Use forecast cache and add bounds query for getting hirlam forecast
    Bounds query returns all pre-read forecast grid points from the grib
    file that are inside given bounds.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var cors = require('cors')
 var Promise = require('bluebird')
+var _ = require('lodash')
 var FMIAPIKey = process.env.FMI_API_KEY || require('./apikey').key
 var geocode = require('./server/reverse_geocode.js')
 var gribParser = require('./server/grib_get_parser.js')
 var observations = require('./server/observations.js')(FMIAPIKey)
 var gribDownloader = require('./server/grib_downloader')
+var forecastCache = require('./server/grib_forecast_cache')
 
 var app = express()
 app.set('port', (process.env.PORT || 8000))
@@ -24,9 +26,22 @@ function startServer() {
   })
 
   app.get("/hirlam-forecast", function(req, res, next) {
-    gribParser.getForecastFromGrib(gribDownloader.gribFile, req.query.lat, req.query.lon)
-      .then(function(forecast) { res.json(forecast).end() })
-      .catch(next)
+    if(req.query.bounds && (req.query.lat || req.query.lon)) {
+      res.status(400).json({message: 'Use either bounds or lat & lon, not both!'}).end()
+    } else if(req.query.bounds) {
+      try {
+        var coords = _.map(req.query.bounds.trim().split(','), parseFloat)
+        res.json(forecastCache.getForecasts({ swCorner: { lat: coords[0], lng: coords[1] }, neCorner: { lat: coords[2], lng: coords[3] } }))
+      } catch (e) {
+        next(e)
+      }
+    } else if(req.query.lat && req.query.lon) {
+      gribParser.getForecastFromGrib(gribDownloader.gribFile, req.query.lat, req.query.lon)
+        .then(function(forecast) { res.json(forecast).end() })
+        .catch(next)
+    } else {
+      res.status(400).json({message: 'Either bounds or lat & lon must be given!'}).end()
+    }
   })
 
   app.get("/observations", function(req, res, next) {
