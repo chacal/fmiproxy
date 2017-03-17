7d2e0556faad370e7aefd43f196fb060b6f7a029
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-03-17 17:09:48 +0200

    Start ES6 conversion, use request-promise

--- server/reverse_geocode.ts
+++ server/reverse_geocode.ts
@@ -1,34 +1,33 @@
-var BPromise = require('bluebird')
-var request = BPromise.promisifyAll(require('request'))
+import requestP = require('request-promise')
 var _ = require('lodash')
 var geolib = require('geolib')
 var moment = require('moment')
 var utils = require('./utils')
 var logger = require('./logging.js').console
 var xpath = require('xpath')
-var dom = require('xmldom').DOMParser
+import { DOMParser } from 'xmldom'
 
-var observationStations = []
+let observationStations = []
 
 function init(apiKey) {
   logger.info("Updating observation station cache..")
   var lastFullHour = moment().minutes(0).seconds(0).utc().format("YYYY-MM-DDTHH:mm:ss") + "Z"
   var observationsUrl = 'http://data.fmi.fi/fmi-apikey/' + apiKey + '/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::obsstations::multipointcoverage&parameters=temperature&starttime=' + lastFullHour + '&endtime=' + lastFullHour
 
-  return request.getAsync(observationsUrl)
-    .then(function(res) {
-      var doc = new dom().parseFromString(res.body.toString())
-      var select = xpath.useNamespaces({
+  return requestP.get(observationsUrl)
+    .then(function(body) {
+      const doc = new DOMParser().parseFromString(body.toString())
+      const select = xpath.useNamespaces({
         target: 'http://xml.fmi.fi/namespace/om/atmosphericfeatures/1.0',
         gml: 'http://www.opengis.net/gml/3.2',
         xlink: 'http://www.w3.org/1999/xlink'
       })
-      var locationNodes = select("//target:Location", doc)
-      return _.map(locationNodes, function(locationNode) {
-        var name = select('./gml:name[@codeSpace="http://xml.fmi.fi/namespace/locationcode/name"]/text()', locationNode).toString()
-        var geoid = select('./gml:name[@codeSpace="http://xml.fmi.fi/namespace/locationcode/geoid"]/text()', locationNode).toString()
-        var poinRef = select('./target:representativePoint/@xlink:href', locationNode, true).value.substr(1)
-        var position = select('//gml:Point[@gml:id="' + poinRef + '"]/gml:pos/text()', doc, true).toString()
+      const locationNodes = select("//target:Location", doc)
+      return locationNodes.map(locationNode => {
+        const name = select('./gml:name[@codeSpace="http://xml.fmi.fi/namespace/locationcode/name"]/text()', locationNode).toString()
+        const geoid = select('./gml:name[@codeSpace="http://xml.fmi.fi/namespace/locationcode/geoid"]/text()', locationNode).toString()
+        const poinRef = select('./target:representativePoint/@xlink:href', locationNode, true).value.substr(1)
+        const position = select('//gml:Point[@gml:id="' + poinRef + '"]/gml:pos/text()', doc, true).toString()
         return _.extend( { geoid: geoid, name: name }, utils.locationFromPositionString(position))
       })
     })
